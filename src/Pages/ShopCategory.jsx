8c3ce1d60db97e580649d1a2cab83129234cf2c3
Murugavel E <murugavele1@gmail.com>
2024-02-17 01:12:33 +0530

    Updated

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-
-const ShopCategory = () => {
+import React, { useContext } from 'react'
+import './css/ShopCategory.css'
+import {ShopContext} from '../Context/ShopContext'
+const ShopCategory = (props) => {
+  const {all_products} = useContext(ShopContext);
   return (
-    <div>
-      
+    <div className='shop-category'>
+      <img src={props.banner} alt="" />
+      <div className="shopcategory-indexSort">
+
+      </div>
     </div>
   )
 }
